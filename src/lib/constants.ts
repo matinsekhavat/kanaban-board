028a409303263c20791afa301510ede01edcab90
matinsekhavat <matinhyphen@gmail.com>
2025-03-13 20:04:23 +0330

    feat(constants): add SORT_CONSTANTS for sorting options with default value

---
+++ src/lib/constants.ts
@@ -0,0 +1,11 @@
+import { SortOption } from '@/types/types';
+
+export const SORT_CONSTANTS = {
+  DEFAULT_SORT: 'name-asc',
+  OPTIONS: [
+    { label: 'A - Z', value: 'name-asc' },
+    { label: 'Z - A', value: 'name-desc' },
+    { label: 'Newest', value: 'date-desc' },
+    { label: 'Oldest', value: 'date-asc' },
+  ] as const satisfies readonly SortOption[],
+} as const;
