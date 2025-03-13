a3b34694dfa945abb473957bd4a5aed020458f6e
matinsekhavat <matinhyphen@gmail.com>
2025-03-13 12:48:53 +0330

    fix(theme): update import path for ThemeContext types

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { createContext, useContext, useEffect, useState } from "react";
-import { type ThemeContextValue, type ThemeLiteral } from "../../types/types";
+import { type ThemeContextValue, type ThemeLiteral } from "../types/types";
 
 const defaultThemeContext: ThemeContextValue = {
   theme: "light",
