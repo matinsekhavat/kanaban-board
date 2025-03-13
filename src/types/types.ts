1282a3c637d934bd46cce326dbc6fe805778714a
matinsekhavat <matinhyphen@gmail.com>
2025-03-13 20:03:19 +0330

    feat(types): add SortOption type definition and standardize ThemeLiteral quotes

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,12 @@
-export type ThemeLiteral = "light" | "dark";
+export type ThemeLiteral = 'light' | 'dark';
 export type ThemeContextValue = {
   theme: ThemeLiteral;
   setTheme: (theme: ThemeLiteral) => void;
   toggleTheme: () => void;
+};
+
+// Types definition
+export type SortOption = {
+  label: string;
+  value: string;
 };
