8f0b6adedaf22ec9ade1b8f782c25b53c339cfc2
matinsekhavat <matinhyphen@gmail.com>
2025-03-13 22:20:32 +0330

    refactor(kanban-main): simplify sort option handling and integrate KanbanList component for task display

--- src/components/kanban/KanabanMain.tsx
+++ src/components/kanban/KanabanMain.tsx
@@ -2,13 +2,18 @@
 import React, { useState, useCallback } from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from '../ui/card';
 import { ChevronDownIcon, Sparkles as SparklesIcon } from 'lucide-react';
-import { DropdownMenuItem, DropdownMenuContent, DropdownMenuTrigger } from '../ui/dropdown-menu';
-import { DropdownMenu } from '../ui/dropdown-menu';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu';
 import { Button } from '../ui/button';
 import { useUpdateSearchParam } from '@/hooks/use-searchParams';
 import { SORT_CONSTANTS } from '@/lib/constants';
 import { SortOption } from '@/types/types';
 import { cn } from '@/lib/utils';
+import KanbanList from './KanbanList';
 
 const findSortOption = (value: string): SortOption => {
   return (
@@ -19,30 +24,23 @@ const findSortOption = (value: string): SortOption => {
 };
 
 function KanbanMain() {
-  const [isSortDropdownOpen, setIsSortDropdownOpen] = useState(false);
-
   const { OPTIONS, DEFAULT_SORT } = SORT_CONSTANTS;
-
   const { getSearchParam, updateSearchParam } = useUpdateSearchParam();
   const [sortBy, setSortBy] = useState(() => getSearchParam('sort') || DEFAULT_SORT);
   const { label } = findSortOption(sortBy);
 
   const handleSortOptionClick = useCallback(
-    (option: SortOption, e: React.MouseEvent) => {
+    (option: SortOption) => {
       const value = option.value;
-      if (value === sortBy) {
-        e.preventDefault();
-        e.stopPropagation();
-        return;
+      if (value !== sortBy) {
+        setSortBy(value);
+        updateSearchParam('sort', value);
       }
-      setSortBy(value);
-      updateSearchParam('sort', value);
     },
     [sortBy, setSortBy, updateSearchParam]
   );
 
   return (
-    // flex-1
     <div className="my-10 grid grid-cols-1 gap-6 md:grid-cols-[1fr_350px]">
       {/* left */}
       <div className="order-2 md:order-1">
@@ -57,10 +55,7 @@ function KanbanMain() {
             {/* HEADER > RIGHT */}
             <div className="flex items-center gap-2">
               <span className="text-sm">Sort by</span>
-              <DropdownMenu
-                open={isSortDropdownOpen}
-                onOpenChange={(open) => setIsSortDropdownOpen(open)}
-              >
+              <DropdownMenu>
                 <DropdownMenuTrigger
                   asChild
                   className="min-w-[100px] justify-between focus-visible:ring-0"
@@ -68,25 +63,17 @@ function KanbanMain() {
                   <Button size={'sm'}>
                     {label}
                     <ChevronDownIcon
-                      className={cn(
-                        'ml-1 size-4 transition-transform duration-200',
-                        isSortDropdownOpen ? 'rotate-180' : 'rotate-0'
-                      )}
+                      className={cn('ml-1 size-4 transition-transform duration-200')}
                     />
                   </Button>
                 </DropdownMenuTrigger>
-                <DropdownMenuContent
-                  align="end"
-                  sideOffset={4}
-                  className="animate-in slide-in-from-top-2 w-[140px] p-1 duration-150"
-                >
+                <DropdownMenuContent align="end" sideOffset={4} className="w-[140px] p-1">
                   {OPTIONS.map((option) => (
                     <DropdownMenuItem
                       key={option.value}
-                      onClick={(e) => handleSortOptionClick(option, e)}
+                      onClick={() => handleSortOptionClick(option)}
                       className={cn('my-1 text-xs transition-colors duration-150 sm:text-sm', {
-                        'bg-primary text-primary-foreground cursor-not-allowed':
-                          option.value === sortBy,
+                        'bg-primary text-primary-foreground': option.value === sortBy,
                       })}
                     >
                       {option.label}
@@ -96,8 +83,10 @@ function KanbanMain() {
               </DropdownMenu>
             </div>
           </CardHeader>
+          <CardContent>
+            <KanbanList />
+          </CardContent>
         </Card>
-        {/* TASKS */}
       </div>
       {/* right */}
       <div className="order-1 md:order-2">
